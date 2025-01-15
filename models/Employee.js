217a332d91fbb55cc5887b3cce33b6e205f92388
bachalimed <69384160+bachalimed@users.noreply.github.com>
2025-01-15 14:17:45 +0300

    Update Employee.js

--- models/Employee.js
+++ models/Employee.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
-
 function capitalizeFirstLetter(str) {
-	if (typeof str !== "string" || str.length === 0) return str;
-	return str.charAt(0).toUpperCase() + str.slice(1);
-  }
+  if (typeof str !== "string" || str.length === 0) return str;
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 const employeeYearsSchema = new mongoose.Schema(
   {
@@ -17,10 +16,31 @@ const employeeAssessmentSchema = new mongoose.Schema(
   {
     employeeAssessment: {
       date: { type: Date },
-      assessor: { type: String,  },
+      assessor: { type: String },
       assessmentComment: { type: String },
       assessmentScore: { type: String },
     },
+  },
+  { _id: false }
+);
+const salaryPackageSchema = new mongoose.Schema(
+  {
+    salaryFrom: { type: Date, required: true },
+    salaryTo: { type: Date, default: null },
+    basicSalary: { type: String, required: true },
+    deduction: {
+      deductionLabel: { type: String },
+      deductionAmount: { type: String },
+    },
+    allowances: [
+      {
+        allowanceLabel: { type: String, set: capitalizeFirstLetter },
+        allowanceUnitValue: { type: String },
+        allowancePeriodicity: { type: String },
+      },
+    ],
+
+    // paymentPeriod: { type: String, required: true },//monthly, hourly
   },
   { _id: false }
 );
@@ -28,32 +48,25 @@ const employeeAssessmentSchema = new mongoose.Schema(
 const employeeWorkHistorySchema = new mongoose.Schema(
   {
     employeeWorkHistory: {
-		institution:{type: String,  set: capitalizeFirstLetter,},
+      institution: { type: String, set: capitalizeFirstLetter },
       fromDate: { type: Date },
       toDate: { type: Date },
-      position: { type: String,  set: capitalizeFirstLetter, },
-      contractType: { type: String,  set: capitalizeFirstLetter, },
-      salaryPackage: {type: String },
-	 
+      position: { type: String, set: capitalizeFirstLetter },
+      contractType: { type: String, set: capitalizeFirstLetter },
+      salaryPackage: { type: String },
     },
   },
   { _id: false }
 );
 
 const employeeSchema = new mongoose.Schema({
-  	employeeCurrentEmployment: {
-		position: { type: String, required: true, set: capitalizeFirstLetter, },
-		employeeColor: { type: String, default:'#597833' },
-		joinDate: { type: Date, required: true },
-		contractType: { type: String, required: true, set: capitalizeFirstLetter, },
-		salaryPackage: { 
-			payment: { type: String, required: true },
-			basic:{type: String, required: true },
-			
-			other:{type: String },
-			allowance:{type: String },
-	 },
+  employeeCurrentEmployment: {
+    position: { type: String, required: true, set: capitalizeFirstLetter },
+    employeeColor: { type: String, default: "#597833" },
+    joinDate: { type: Date, required: true },
+    contractType: { type: String, required: true, set: capitalizeFirstLetter },
   },
+  salaryPackage: [salaryPackageSchema],
   employeeIsActive: { type: Boolean },
   employeeYears: [employeeYearsSchema],
   employeeAssessment: [employeeAssessmentSchema],
